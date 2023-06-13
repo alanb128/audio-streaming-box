585b33bf3cd64783eb6b00ff3204be91e1f9cd68
Alan Boris <39385685+alanb128@users.noreply.github.com>
2023-06-13 01:26:57 -0400

    Update flask-api.py

--- controller/flask-api.py
+++ controller/flask-api.py
@@ -10,21 +10,26 @@ from adafruit_seesaw import seesaw, rotaryio
 import RPi.GPIO as GPIO
 
 import requests
+import os
 
 VERSION = 0.5
 DISPLAY_RESUME = 2.66  # seconds
 
 # Adafruit I2C QT Rotary Encoder
 # Using the INT output on Pi GPIO 17
-seesaw = seesaw.Seesaw(board.I2C(), addr=0x36)
-seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
-print("Found seesaw supported product {}".format(seesaw_product))
-if seesaw_product != 4991:
-    print("Wrong firmware loaded for QT encoder?  Expected 4991")
-# Set up the rotary click button, and add to interrupt
-seesaw.pin_mode(24, seesaw.INPUT_PULLUP)  # Pin on the QT
-seesaw.set_GPIO_interrupts(1 << 24, True)
-seesaw.enable_encoder_interrupt()
+try:
+    seesaw = seesaw.Seesaw(board.I2C(), addr=0x36)
+except:
+    print("No rotary encoder - skipping...")
+else:
+    seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
+    print("Found seesaw supported product {}".format(seesaw_product))
+    if seesaw_product != 4991:
+        print("Wrong firmware loaded for QT encoder?  Expected 4991")
+    # Set up the rotary click button, and add to interrupt
+    seesaw.pin_mode(24, seesaw.INPUT_PULLUP)  # Pin on the QT
+    seesaw.set_GPIO_interrupts(1 << 24, True)
+    seesaw.enable_encoder_interrupt()
 
 rotary_pos = 0  # keeps track of wheel position
 
@@ -75,6 +80,9 @@ playlists = []
 preset_keys = ["T", "N", "J", "E", "A", "U", "P", "K", "F", "B"]
 current_playlist = ""  # current or most recently played playlist
 sel_playlist = ""   # selected playlist before being played
+last_key = ""  # help with double key press issue
+last_key_count = 0  # also help with double key
+current_volume = 0
 
 def lcd_display(trigger):
     #
@@ -93,13 +101,59 @@ def lcd_display(trigger):
     elif trigger == "init":
         lcd.message = "     moOde\n     audio"
 
+def sanitizer(mystring):
+    #
+    # removes anything but letters and numbers from input string
+    # strings from moode can contain all kinds of garbage
+    #
+    out_string = ''
+    for i in mystring:
+        s = ord(i)
+        if (s > 31) and (s < 127):
+            out_string = out_string + i
+
+    return out_string
+
+def get_current_state():
+    #
+    # get current state from moode api
+    #
+    global current_song, current_volume
+    
+    r = requests.get('http://host.docker.internal/command/?cmd=get_currentsong')
+    current = r.json()
+    current_song["artist"] = sanitizer(current["artist"])
+    current_song["album"] = sanitizer(current["album"])
+    current_song["title"] = sanitizer(current["title"])
+    current_song["encoded"] = sanitizer(current["encoded"])
+    current_song["bitrate"] = sanitizer(current["bitrate"])
+    current_song["volume"] = sanitizer(current["volume"])
+    current_song["mute"] = sanitizer(current["mute"])
+    current_song["state"] = sanitizer(current["state"])
+
+    current_volume = int(current_song["volume"])
+
+def key_handler_x(key):
+    #
+    # Since every key gets tow presses for some reason,
+    # this module fixes that
+    #
+    #global last_key, last_key_count
+
+    #if key != last_key:
+    #    key_supervisor(key)
+    #    print("-- accepting keypress {0}, {1}".format(key, last_key))
+
+    #last_key = key
+    print("key: {}".format(key))
+
 def key_handler(key):
     #
     # Catches any key presses and performs appropriate action
     # T N J E A U P K F B H
     #
     global sel_playlist, current_playlist
-    if key in preset_keys:
+    if key in preset_keys:   # select a playlist
         if preset_keys.index(key) + 1 > len(playlists):
             lcd.clear()
             lcd.message = "No playlist\ndefined..."
@@ -109,12 +163,18 @@ def key_handler(key):
             sel_playlist = playlists[preset_keys.index(key)]
             lcd.clear()
             lcd.message = "Play playlist?\n" + playlists[preset_keys.index(key)]
-    elif key == "H":
+    elif key == "D":    # play/pause or play selected playlist
         if sel_playlist == "":
-            lcd.clear()
-            lcd.message = "No playlist\nselected..."
-            time.sleep(DISPLAY_RESUME)
-            lcd_display("resume")  
+            get_current_state()
+            if current_song["state"] == "play":
+                # pause
+                r = requests.get('http://host.docker.internal/command/?cmd=pause')
+                print("from PLAY state to PAUSE")
+            elif current_song["state"] == "stop" or current_song["state"] == "pause":
+                # play
+                r = requests.get('http://host.docker.internal/command/?cmd=play')
+                print("From {} to PLAY".format(current_song["state"]))
+            lcd_display("resume")
         else:
             lcd.clear()
             lcd.message = "Please wait..."
@@ -122,6 +182,31 @@ def key_handler(key):
             current_playlist == sel_playlist
             sel_playlist = "" 
             lcd_display("resume")   
+    elif key =="V":
+        pass
+
+def adj_vol(direction):
+    #
+    #  changes volume level - expects "left" or "right"
+    #
+    global current_volume
+
+    vol_level = ""
+    v = round(current_volume/15) + 1
+    print("volume: {0} - {1}".format(v, current_volume))
+    #for i in range(v):
+    #     vol_level = vol_level + "\x03"
+    vol_level = "\x03" * v
+    #print("volume: {}".format(v))
+    vol_level = vol_level.ljust(16)
+    #print("vol level: {}".format(vol_level))
+    lcd.message = "Volume          \n" + vol_level
+    if direction == "left":
+        current_volume = current_volume - 1
+        r = requests.get('http://host.docker.internal/command/?cmd=vol.sh%20-dn%201')
+    else:
+        current_volume = current_volume + 1
+        r = requests.get('http://host.docker.internal/command/?cmd=vol.sh%20-up%201')
 
 def rotary_incoming(r):
     #
@@ -142,15 +227,13 @@ def rotary_incoming(r):
             #lcd.message = "Rotary right: {}".format(current_pos)
             # Action on every other (even) click
             if (current_pos % 2) == 0:
-                #wheel_right()
-                pass
+                adj_vol("right")
         else:
             print("Turned left {}".format(current_pos))
             #lcd.message = "Rotary left: {}".format(current_pos)
             # Action on every other (even) click
             if (current_pos % 2) == 0:
-                #wheel_left()
-                pass
+                adj_vol("left")
     rotary_pos = current_pos
     #print("seesaw rotary data updated! {0}, {1}".format(rot_pos, rot_btn))
 
@@ -193,40 +276,46 @@ def get_api():
 @app.route('/', methods=['POST'])
 def post_api():
 
-    global current_song
+    global current_song, current_volume
     
-    current_song["artist"] = request.form["artist"]
-    current_song["album"] = request.form["album"]
-    current_song["title"] = request.form["title"]
-    current_song["encoded"] = request.form["encoded"]
-    current_song["bitrate"] = request.form["bitrate"]
-    current_song["volume"] = request.form["volume"]
-    current_song["mute"] = request.form["mute"]
-    current_song["state"] = request.form["state"]
-    
+    current_song["artist"] = sanitizer(request.form["artist"])
+    current_song["album"] = sanitizer(request.form["album"])
+    current_song["title"] = sanitizer(request.form["title"])
+    current_song["encoded"] = sanitizer(request.form["encoded"])
+    current_song["bitrate"] = sanitizer(request.form["bitrate"])
+    current_song["volume"] = sanitizer(request.form["volume"])
+    current_song["mute"] = sanitizer(request.form["mute"])
+    current_song["state"] = sanitizer(request.form["state"])
+
+    current_volume = int(current_song["volume"])
+    print("set vol: {0} - {1}".format(current_song["volume"], current_volume))
     lcd_display("post")
     
     return '', 204
 
 
 #  main thread of execution
-if __name__=='__main__':
 
-    lcd_display("init")
-    
-    playlists = get_playlists()
-    
-    #rotaryio interrupts
-    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(17, GPIO.FALLING, callback=rotary_incoming)
+# Turn backlight on
+lcd.backlight = True
 
-    # key_handler will be called each time a keypad button is pressed
-    keypad.registerKeyPressHandler(key_handler)
+lcd_display("init")
 
-    # Turn backlight on
-    lcd.backlight = True
+get_current_state()
 
-    # start API server
-    app.run(host="0.0.0.0",port=5000,debug=True)
+playlists = get_playlists()
+
+#rotaryio interrupts
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=rotary_incoming)
+
+# key_handler will be called each time a keypad button is pressed
+keypad.registerKeyPressHandler(key_handler)
+
+lcd_display("post")
+
+# start API server
+app.run(host="0.0.0.0",port=5000,debug=True,use_reloader=False)
+
 
 
